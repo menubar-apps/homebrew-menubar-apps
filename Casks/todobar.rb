f36db844adfbb6e6aeb5391fa03d14dc7c8255dd
streetturtle <streetturtle@users.noreply.github.com>
2023-07-08 02:10:09 +0000

    todobar 1.2

--- Casks/todobar.rb
+++ Casks/todobar.rb
@@ -1,11 +1,11 @@
 cask "todobar" do
-    version "0.0"
-    sha256 "8e126a57a6fc3bc70b7ada62503f969fcc39ce6a39d2c9bd7b1cad465c02ba6a"
-  
-    url "https://github.com/menubar-apps/ToDoBar/releases/download/v#{version}/ToDoBar.#{version}.dmg"
-    name "todobar"
-    desc "No BS todo menubar app!"
-    homepage "https://github.com/menubar-apps/ToDoBar"
-  
-    app "todobar.app"
-  end
+  version "1.2"
+  sha256 "30d28dc86dd0eccd1e006b746547696ff735afe4553b6482893186756a5d2017"
+
+  url "https://github.com/menubar-apps/ToDoBar/releases/download/v#{version}/ToDoBar.#{version}.dmg"
+  name "todobar"
+  desc "No BS todo menubar app!"
+  homepage "https://github.com/menubar-apps/ToDoBar"
+
+  app "todobar.app"
+end
