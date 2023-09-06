e55a9d49b0b19bfdd00c99072da6e3abc7310aa4
streetturtle <streetturtle@users.noreply.github.com>
2023-09-06 08:13:41 -0400

    Update streakbar.rb

--- Casks/streakbar.rb
+++ Casks/streakbar.rb
@@ -1,6 +1,6 @@
 cask "streakbar" do
   version "1.0"
-  sha256 "866b9846e5697922cf61fbd980b14f6adce6c416"
+  sha256 "7f38890eea9aa892b632baaacfdde83226d5bd88836a8dace6c8c86ab98ff3db"
 
   url "https://github.com/menubar-apps/StreakBar/releases/download/v#{version}/streak-bar.#{version}.dmg"
   name "streakbar"
