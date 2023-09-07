18e1b644b494eb18d55d2dfd883a4b4dca0b19dd
streetturtle <streetturtle@users.noreply.github.com>
2023-09-06 22:03:14 -0400

    Update streakbar.rb

--- Casks/streakbar.rb
+++ Casks/streakbar.rb
@@ -2,7 +2,7 @@ cask "streakbar" do
   version "1.0"
   sha256 "7f38890eea9aa892b632baaacfdde83226d5bd88836a8dace6c8c86ab98ff3db"
 
-  url "https://github.com/menubar-apps/StreakBar/releases/download/v#{version}/streak-bar.#{version}.dmg"
+  url "https://github.com/menubar-apps/StreakBar/releases/download/v#{version}/StreakBar.#{version}.dmg"
   name "streakbar"
   desc "GitHub contributions chart in you menu bar"
   homepage "https://github.com/menubar-apps/StreakBar"
