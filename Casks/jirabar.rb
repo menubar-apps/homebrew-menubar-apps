9ed3ddbd2174531c1a456559594f35a6dc2c5044
streetturtle <streetturtle@users.noreply.github.com>
2023-10-30 18:29:10 -0400

    Update jirabar.rb

--- Casks/jirabar.rb
+++ Casks/jirabar.rb
@@ -3,7 +3,7 @@ cask "jirabar" do
   sha256 "7e66cd70f5f237dd1c7baf9cdfa83b9c56a6bcad02dfa95b6864e71861a83b48"
 
   url "https://github.com/menubar-apps/JiraBar/releases/download/v#{version}/jiraBar.#{version}.dmg"
-  name "pullbar"
+  name "jirabar"
   desc "Access your Jira issues from you menu bar!"
   homepage "https://github.com/menubar-apps/JiraBar"
 
