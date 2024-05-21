2a077eb074708a6732345cecdfb677126ff507b4
streetturtle <streetturtle@users.noreply.github.com>
2024-05-21 10:04:53 -0400

    Update who-is-missing.rb

--- Casks/who-is-missing.rb
+++ Casks/who-is-missing.rb
@@ -1,5 +1,5 @@
 cask "who-is-missing" do
-  version "1.b0bcbc8c86c1002b922a6f3655282b913dcc789bf48f601346eed1a987431dc6"
+  version "1.0"
   sha256 "b0bcbc8c86c1002b922a6f3655282b913dcc789bf48f601346eed1a987431dc6"
 
   url "https://github.com/menubar-apps/who-is-missing/releases/download/v#{version}/Who.s.Missing.#{version}.dmg"
