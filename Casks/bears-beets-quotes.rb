ee538c98333acacf68cecc5e6559fe12f2fb8440
streetturtle <streetturtle@users.noreply.github.com>
2025-03-23 21:36:04 -0400

    Update bears-beets-quotes.rb

--- Casks/bears-beets-quotes.rb
+++ Casks/bears-beets-quotes.rb
@@ -2,7 +2,7 @@ cask "bears-beets-quotes" do
   version "1.0"
   sha256 "1"
 
-  url "https://github.com/menubar-apps/ToDoBar/releases/download/v#{version}/Bears.Beets.Quotes.#{version}.dmg"
+  url "https://github.com/menubar-apps/bears-beets-quotes/releases/download/v#{version}/Bears.Beets.Quotes.#{version}.dmg"
   name "Bears, Beets, Quotes!"
   desc "Quotes from The Office show, a click away!"
   homepage "https://github.com/menubar-apps/bears-beets-quotes"
