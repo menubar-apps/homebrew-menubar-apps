bd68e8a9624d6a1d24b334b4cfd405c2d7ef7145
streetturtle <streetturtle@users.noreply.github.com>
2025-12-06 18:54:24 +0000

    todobar 1.5

--- Casks/todobar.rb
+++ Casks/todobar.rb
@@ -1,6 +1,6 @@
 cask "todobar" do
-  version "1.4"
-  sha256 "7265ca7cfb9d5d08ba7c3db4979efc133b078965ef4d3bdba8cfdfd8ae7ca6d5"
+  version "1.5"
+  sha256 "53bda080c62129963e637d3f8ad7b2a4a77c0d48036f985fc79c37d6ed6d14a3"
 
   url "https://github.com/menubar-apps/ToDoBar/releases/download/v#{version}/ToDoBar.#{version}.dmg"
   name "todobar"
